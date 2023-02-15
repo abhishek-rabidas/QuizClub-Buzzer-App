5234a3d2ad94c70c03cc56cf4f3180da7401b173
abhishek <avisec07@gmail.com>
2023-02-15 14:00:16 +0530

    initial files add

---
+++ src/main/java/quiz/club/silicon/QuizClub/QuizClubApplication.java
@@ -0,0 +1,13 @@
+package quiz.club.silicon.QuizClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quiz/club/silicon/QuizClub/QuizClubApplicationTests.java
@@ -0,0 +1,13 @@
+package quiz.club.silicon.QuizClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
